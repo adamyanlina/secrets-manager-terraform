d5584bd575f57c3929a1aff60bea0507b6f8f46e
Lina Adamyan <adamyanlina97@gmail.com>
2022-05-05 21:12:53 +0400

    Do dynamic resource

--- modules/secrets-manager/secrets-manager.tf
+++ modules/secrets-manager/secrets-manager.tf
@@ -1,3 +1,22 @@
-resource "aws_secretsmanager_secret" "secrets-manager" {
-  name = var.secrets_manager_name
+resource "aws_secretsmanager_secret" "secrets_manager" {
+  for_each    = var.secrets_map
+  name        = each.value["secret_name"]
+  description = "Managed by Terraform"
+}
+
+resource "aws_secretsmanager_secret_version" "secret" {
+  for_each = {
+  for key, value in var.secrets_map :
+  key => value
+  if lookup(value, "secret_value", "") != ""
+  }
+  secret_id     = each.value["secret_name"]
+  secret_string = <<EOF
+   {
+    "${each.value["secret_value"]}": "${each.value["secret_key"]}"
+   }
+EOF
+  depends_on = [
+    aws_secretsmanager_secret.secrets_manager
+  ]
 }
--- modules/secrets-manager/variables.tf
+++ modules/secrets-manager/variables.tf
@@ -1,4 +1,7 @@
-variable "secrets_manager_name" {
-  type = string
-  description = "Secrets Manager Name"
+variable "secrets_map" {
+  type = map(object({
+    secret_name  = string
+    secret_value = string
+    secret_key   = string
+  }))
 }
