21609408457abd4f97aa8adeebfcc7c3ddace371
Lina Adamyan <adamyanlina97@gmail.com>
2022-05-09 14:54:48 +0400

    add example

---
+++ example-usage.tf
@@ -0,0 +1,31 @@
+module "secrets-manager" {
+  source = "git::https://github.com/adamyanlina/secrets-manager-terraform.git//modules/secrets-manager"
+
+  secrets = {
+    mongodb-uhkv = {
+      description = "MongoDB URI & Host secrets"
+      secret_key_value = {
+        uri = "mongodb://username:pass/example.com"
+        host = "localhost"
+      }
+      recovery_window_in_days = 7
+    },
+    mongodb-upkv = {
+      description = "MongoDB Username & Password secrets"
+      secret_key_value = {
+        username = "muser"
+        password = "topsecret"
+      }
+      tags = {
+        app = "mongodbuser"
+      }
+      recovery_window_in_days = 7
+    },
+  }
+
+  tags = {
+    Owner       = "Lina"
+    Environment = "dev"
+    Terraform   = true
+  }
+}
